58cbca815b4919695a5dd8f6c888dc1b76ee1d38
aarajh <aarajh@gmail.com>
2019-12-08 13:37:44 -0600

    Styles Fields

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -4,9 +4,12 @@ import "typeface-roboto";
 
 const Field = props => (
     <div className={props.className} data-testid="field">
+        <div className={props.labelClass}>
         {props.label}
+        </div>
     <input 
         data-testid="inputfield"
+        className={props.innerClass}
         type={props.type} 
         name={props.name}
         onChange={props.onChange}
@@ -22,6 +25,8 @@ Field.defaultProps = {
     name: 'Field',
     type: 'text',
     value: '',
+    innerClass: 'DefaultInnerClass',
+    labelClass: 'DefaultLabelClass',
     className: 'DefaultFieldClass',
     onChange: () => {},
 }
