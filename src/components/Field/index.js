9b0877e9a9b24abdf7e3ee975a987cda564512c3
aarajh <aarajh@gmail.com>
2019-12-08 16:01:32 -0600

    Added field validation rules Error Message Formatting fixed Used html forms for signup

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import './Field.css'
 import "typeface-roboto";
 
+const ShowError = error =>(
+    <div style={error?{}:{visibility:'hidden'}} className="ErrorField">
+        {error}
+    </div>
+)
+
 const Field = props => (
     <div className={props.className} data-testid="field">
         <div className={props.labelClass}>
         {props.label}
+        {ShowError(props.error)}
         </div>
     <input 
         data-testid="inputfield"
         className={props.innerClass}
+        style={props.error?{borderColor:"#df0d6f52"}:null}
         type={props.type} 
         name={props.name}
         onChange={props.onChange}
@@ -24,7 +32,8 @@ Field.defaultProps = {
     label: 'Field',
     name: 'Field',
     type: 'text',
-    value: '',
+    value: null,
+    error: null,
     innerClass: 'DefaultInnerClass',
     labelClass: 'DefaultLabelClass',
     className: 'DefaultFieldClass',
